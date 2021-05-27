3e5b1f0146a53e9430fde661ba17b0a6dcd24a00
yura <yura04091@gmail.com>
2021-05-27 14:23:36 +0300

    style_db

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,4 +1,4 @@
-// import checkNumInputs from './checkNumInputs';
+import {postData} from '../services/requests';
 
 const forms = () => {
     const form = document.querySelectorAll('form'),
@@ -23,15 +23,7 @@ const forms = () => {
         question: 'assets/question.php'
     };
 
-    const postData = async (url, data) => {
-        
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
-
-        return await res.text();
-    };
+    
 
     const clearInputs = () => {
         inputs.forEach(item => {
